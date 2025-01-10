1feac2884187b5126e0a2d7ac209d3e055e61636
Nofer <sxx261@case.edu>
2025-01-10 18:49:27 -0500

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -8,16 +8,8 @@ project "ImGUI"
 	objdir ("bin-obj/" .. outputdir .. "/%{prj.name}")
 
 	files{
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"*.h",
+		"*.cpp"
 	}
 
 	filter "system:windows"
